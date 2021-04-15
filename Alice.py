ceb66bb1f8e919649591d4cdceac0a35e5cad923
Sean <seanzhao2@gmail.com>
2021-04-14 21:08:19 -0400

    finished

--- Alice.py
+++ Alice.py
@@ -58,7 +58,8 @@ if __name__ == '__main__':
         line = line.strip()
         content = line.split(",")
         initial_point = content[0].split(".")
-        d[Point(int(initial_point[0]), int(initial_point[1]))] = content[1:]
+        d[Point(int(initial_point[0]), int(initial_point[1])).__str__()] = content[1:]
+        d[Point(int(initial_point[0]), int(initial_point[1])).__str__()].append(Point(int(initial_point[0]), int(initial_point[1])))
 
     start = None
     for i in d:
@@ -70,14 +71,17 @@ if __name__ == '__main__':
         if (d[i][0] != 'GOAL'):
             # split all the neighbour vectors into a list
             d[i][1] = d[i][1].split("|")
+        else:
+            goal = i
 
         # convert all neighbour vectors into Point objects
         for j in range(len(d[i][1])):
             vector = d[i][1][j].split(".")
             d[i][1][j] = Point(int(vector[0]), int(vector[1]))
 
-        d[i][-1] = int(d[i][-1])
-        d[i][-3] = None
+        d[i][-2] = int(d[i][-2])
+        d[i][-4] = None
+
 
     maze_size = int(len(d) ** 0.5)
 
@@ -87,7 +91,6 @@ if __name__ == '__main__':
     adj_list = {}
     temp = step
 
-    print("about to step into while loop")
     while (temp != maze_size):
         inner = {}
 
@@ -95,15 +98,13 @@ if __name__ == '__main__':
             lst = []
             for j in range(len(d[i][1])):
 
-                lst.append(i.step(d[i][1][j], temp))
+                lst.append(d[i][-1].step(d[i][1][j], temp))
 
             inner[i] = lst
             adj_list[temp] = inner
 
         temp += 1
-    # print(type(adj_list.keys()[0]))
-
-
+    print(adj_list)
 
     # initialize queue with start
     step = 1
@@ -115,45 +116,46 @@ if __name__ == '__main__':
     while q.isEmpty() != True:
         u = q.dequeue()
 
-        if (d[u][0] == 'RED'):
+        if (d[u.__str__()][0] == 'RED'):
             step += 1
-        elif (d[u][0] == 'YELLOW'):
+        elif (d[u.__str__()][0] == 'YELLOW'):
             step -= 1
 
-        if (step == maze_size or step == 0):
+        if (step == maze_size or step == 0 or failed == 1):
             failed = 1
             break
 
-        print("about to go into adj_list")
-        print(d)
-        print(d[Point(0,2)])
-
-        for i in adj_list[step][u]:
-
-            if d[i][3] == 'white':
+        for i in adj_list[step][u.__str__()]:
+            try:
+                if d[i.__str__()][3] == 'white':
 
                 # change the colour
-                d[i][3] = 'grey'
-
-                # increase the distance
-                d[i][4] += 1
+                    d[i.__str__()][3] = 'grey'
 
                 # update the parent
-                d[i][2] = u
+                    d[i.__str__()][2] = u
 
-                q.enqueue(i)
+                # increase the distance
+                    d[i.__str__()][4] = d[d[i.__str__()][2].__str__()][4] + 1
 
-        d[u][3] = 'black'
+                    q.enqueue(i)
+            except(KeyError):
+                failed = 1
+                break
+        d[u.__str__()][3] = 'black'
 
     if (failed == 1):
-        print("Failed.")
+        print("No sol")
     else:
         # print path here
-        print("Good")
-
-
-
-
+        last = []
+        dist = d[goal][4]
+        last.append(d[goal][-1])
+        while d[goal][2] is not None:
+            last.append(d[goal][2])
+            goal = str(d[goal][2])
+        print(last)
+        print(dist)
 
 
 
